60a63d8827eb7a146f3aa396cd469f4d4db32917
82108 <dudals789@naver.com>
2023-06-19 20:44:15 +0900

    팩토리 패턴에서의 신규 기능 추가

---
+++ src/main/java/com/example/factorypattern/service/MultiplyService.java
@@ -0,0 +1,24 @@
+package com.example.factorypattern.service;
+
+import com.example.factorypattern.enums.Type;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MultiplyService implements TestService{
+
+    @Override
+    public int execute() {
+        int sum = 1;
+
+        for(int a = 1; a <= 10; a++){
+            sum*=a;
+        }
+
+        return sum;
+    }
+
+    @Override
+    public Type getType() {
+        return Type.MULTIPLY;
+    }
+}
